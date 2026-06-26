433497274551d13226e86d1198b407d218aaf707
akshay30rwt <akshayrwt8700@gmail.com>
2026-06-26 20:08:43 +0530

    Add GET, POST, PUT, and DELETE routes for Books API

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3000;
+
+let books = [];
+let nextId = 1;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Contacts API is running'
+    });
+});
+
+app.get('/books', (req, res) => {
+    if(books.length === 0) {
+        return res.status(404).json({
+            message: 'There are no books here'
+        });
+    }
+
+    res.status(200).json(books);
+});
+
+app.post('/books', (req, res) => {
+    const { title, genre } = req.body;
+    const newBook = {
+        id: nextId,
+        title: title, 
+        genre: genre
+    }
+    nextId++;
+
+    books.push(newBook);
+    res.status(201).json({
+        message: `Title: '${title}' book added to the library`
+    });
+});
+
+app.put('/books/:id', (req, res) => {
+    const { title, genre } = req.body;
+    const bookId = Number(req.params.id);
+    const index = books.findIndex(book => book.id === bookId);
+
+    if(index === -1) {
+        return res.status(404).json({
+            message: `Book with ID: ${bookId} not found`
+        });
+    }
+
+    books[index].title = title;
+    books[index].genre = genre;
+    
+    res.status(200).json({
+        message: `ID: ${bookId} | Title: ${title} updated successfully`
+    });
+});
+
+app.delete('/books/:id', (req, res) => {
+    const bookId = Number(req.params.id);
+    const index = books.findIndex(book => book.id === bookId);
+
+    if(index === -1) {
+        return res.status(404).json({
+            message: `Book with ID: ${bookId} not found`
+        });
+    }
+    const deletedBook = books[index];
+    books.splice(index, 1);
+    res.status(200).json({
+        message: `Book Title: ${deletedBook.title} deleted from the library`
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
